52bade5ad4f7b7b9b39ac02dbefada14fe281d49
Junior Schmidt <junior.schmidt.99@gmail.com>
2018-07-03 13:31:45 -0300

    files

---
+++ vsetting/voice.lua
@@ -0,0 +1,49 @@
+﻿local voiceCols = {}
+local broadcoastTo = {}
+ 
+addEventHandler("onPlayerVoiceStart", root,
+    function()
+        local voiceSource = source
+        local sx, sy, sz = getElementPosition(voiceSource)
+        voiceCols[voiceSource] = createColSphere(sx, sy, sz, 100)
+        attachElements(voiceCols[voiceSource], voiceSource)
+        broadcoastTo[voiceSource] = getElementsWithinColShape(voiceCols[voiceSource], "player")
+        setPlayerVoiceBroadcastTo(voiceSource, broadcoastTo[voiceSource])
+        addEventHandler("onColshapeHit", voiceCols[voiceSource],
+            function(element)
+                if (getElementType(element) == "player") then
+                    table.insert(broadcoastTo[voiceSource], element)
+                    setPlayerVoiceBroadcastTo(voiceSource, broadcoastTo[voiceSource])
+                end
+            end
+        )
+       
+        addEventHandler("onColshapeLeave", voiceCols[voiceSource],
+            function(element)
+                if (getElementType(element) == "player") then
+                    for key, player in pairs(broadcoastTo[voiceSource]) do
+                        if (element == player) then
+                            table.remove(broadcoastTo[voiceSource], key)
+                            break
+                        end
+                    end
+                    setPlayerVoiceBroadcastTo(voiceSource, broadcoastTo[voiceSource])
+                end
+            end
+        )
+    end
+)      
+ 
+addEventHandler("onPlayerVoiceStop", root,
+    function()
+        if isElement(voiceCols[source]) then
+            destroyElement(voiceCols[source])
+        end
+       
+        if (broadcoastTo[source]) then
+            table.remove(broadcoastTo, source)
+        end
+ 
+        setPlayerVoiceBroadcastTo(source)
+    end
+)
